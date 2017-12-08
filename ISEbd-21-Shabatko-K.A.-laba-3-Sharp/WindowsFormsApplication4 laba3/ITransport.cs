374e5a13da8229df404bda324362c79dc07cc425
Xenana <31593457+Xenana@users.noreply.github.com>
2017-12-09 00:19:11 +0400

    Add files via upload

---
+++ ISEbd-21-Shabatko-K.A.-laba-3-Sharp/WindowsFormsApplication4 laba3/ITransport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+    interface ITransport
+    {
+        void flyPlane(Graphics g);
+
+        void drawPlane(Graphics g);
+
+        void setPosition(int x, int y);
+
+        void loadPassenger(int count);
+
+        int getPassenger();
+    }
+}
